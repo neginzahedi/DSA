73f119a0efb8f8ea7cb9059a74d7977600d0d841
Negin Zahedi <zahedi.negin@gmail.com>
2024-08-21 15:49:52 -0400

    Add timeConversion problem with unit test

--- DSATests/HackerRankTests.swift
+++ DSATests/HackerRankTests.swift
@@ -34,10 +34,43 @@ final class HackerRankTests: XCTestCase {
         XCTAssertEqual(result, 15, "Expected 15 but got \(result) for input arr: [[11, 2, 4], [4, 5, 6], [10, 8, -12]]")
     }
     
-    // MARK: -
+    // MARK: - birthdayCakeCandles
     func testBirthdayCakeCandles(){
         // Test Case
         let result = birthdayCakeCandles(candles: [3,2,1,3])
         XCTAssertEqual(result, 2, "Expected 2 but got \(result) for input arr: [3,2,1,3]")
+    }
+    
+    // MARK: - timeConversion
+    func testTimeConversion() {
+        // Test Case 1: Convert 12:00:00AM to 00:00:00
+        let time1 = "12:00:00AM"
+        let expectedOutput1 = "00:00:00"
+        XCTAssertEqual(timeConversion(s: time1), expectedOutput1)
+
+        // Test Case 2: Convert 01:05:45AM to 01:05:45
+        let time2 = "01:05:45AM"
+        let expectedOutput2 = "01:05:45"
+        XCTAssertEqual(timeConversion(s: time2), expectedOutput2)
+
+        // Test Case 3: Convert 12:00:00PM to 12:00:00
+        let time3 = "12:00:00PM"
+        let expectedOutput3 = "12:00:00"
+        XCTAssertEqual(timeConversion(s: time3), expectedOutput3)
+
+        // Test Case 4: Convert 07:05:45PM to 19:05:45
+        let time4 = "07:05:45PM"
+        let expectedOutput4 = "19:05:45"
+        XCTAssertEqual(timeConversion(s: time4), expectedOutput4)
+
+        // Test Case 5: Convert 11:59:59AM to 11:59:59
+        let time5 = "11:59:59AM"
+        let expectedOutput5 = "11:59:59"
+        XCTAssertEqual(timeConversion(s: time5), expectedOutput5)
+
+        // Test Case 6: Convert 11:59:59PM to 23:59:59
+        let time6 = "11:59:59PM"
+        let expectedOutput6 = "23:59:59"
+        XCTAssertEqual(timeConversion(s: time6), expectedOutput6)
     }
 }
