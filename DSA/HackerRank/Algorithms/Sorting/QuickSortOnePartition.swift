7bf9887a64dc67ccb8cf08a5755d9134d2e312e1
Negin Zahedi <zahedi.negin@gmail.com>
2024-08-26 18:34:45 -0400

    Add "Quick Sort Partition" problem

---
+++ DSA/HackerRank/Algorithms/Sorting/QuickSortOnePartition.swift
@@ -0,0 +1,29 @@
+//
+//  QuickSortOnePartition.swift
+//  DSA
+//
+//  Created by Negin Zahedi on 2024-08-26.
+//
+
+import Foundation
+
+/*
+ * Complete the 'quickSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func quickSort(arr: [Int]) -> [Int] {
+    
+    var arr = arr
+    let pivot = arr[0]
+    
+    let left = arr.filter{$0 < pivot}
+    let equal = arr.filter{$0 == pivot}
+    let right = arr.filter{$0 > pivot}
+
+    arr = left + equal + right
+    
+    return arr
+}
