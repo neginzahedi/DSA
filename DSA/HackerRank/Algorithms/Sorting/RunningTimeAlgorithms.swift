7b040e600b8dc01cc0d5fb4f172e35aa1b04a64b
Negin Zahedi <zahedi.negin@gmail.com>
2024-08-26 17:43:16 -0400

    Add "Running Tome of Algorithms" problem

---
+++ DSA/HackerRank/Algorithms/Sorting/RunningTimeAlgorithms.swift
@@ -0,0 +1,35 @@
+//
+//  RunningTimeAlgorithms.swift
+//  DSA
+//
+//  Created by Negin Zahedi on 2024-08-26.
+//
+
+import Foundation
+
+/*
+ * Complete the 'runningTime' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func runningTime(arr: [Int]) -> Int {
+    var arr = arr
+    var counter = 0
+    
+    for i in 1..<arr.count {
+        var current = arr[i]
+        var j = i - 1
+        
+        while j >= 0 && current < arr[j]{
+            arr[j+1] = arr[j]
+            j = j-1
+            counter += 1
+        }
+        
+        arr[j + 1] = current
+    }
+    
+    return counter
+}
