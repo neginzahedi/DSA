90eaf186c4a291761c8b71c3d43c5718621bb546
Negin Zahedi <zahedi.negin@gmail.com>
2024-08-26 13:10:36 -0400

    Add "Intro to Tutorial Challenges" problem

---
+++ DSA/HackerRank/Algorithms/Sorting/IntroTutorial.swift
@@ -0,0 +1,27 @@
+//
+//  IntroTutorial.swift
+//  DSA
+//
+//  Created by Negin Zahedi on 2024-08-26.
+//
+
+import Foundation
+
+/*
+ * Complete the 'introTutorial' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER V
+ *  2. INTEGER_ARRAY arr
+ */
+
+func introTutorial(V: Int, arr: [Int]) -> Int {
+    for i in 0..<arr.count {
+        if arr[i] == V{
+            return i
+        }
+    }
+    
+    return -1
+}
